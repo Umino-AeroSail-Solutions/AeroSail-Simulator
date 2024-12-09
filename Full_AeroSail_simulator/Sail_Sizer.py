f62d4e56cf76897af0507c4c9f322615bc63b80f
Andrés Blanquer <maquinsa101abb@gmail.com>
2024-12-09 12:30:15 +0100

    Added max load analysis

--- Full_AeroSail_simulator/Sail_Sizer.py
+++ Full_AeroSail_simulator/Sail_Sizer.py
@@ -9,8 +9,8 @@ sail_instance = Sail_Class('Data/E473coordinates.txt', 5, 0.4, height=None, pane
 interpolation = 'Data/interpolationCR4sail_XFLR5.npz'
 sail_instance.load_interpolation(interpolation)
 
-Stackheight = 6
-SF=2
+Stackheight = 4
+SF=1.3
 max_windspeedknots = 30
 max_windspeed= max_windspeedknots/1.944
 
@@ -24,6 +24,8 @@ height = initial_height
 chord = height*2/aspectratio
 
 failure = False
+
+maxload = 0
 while not failure:
     chord = height * 2 / aspectratio
     sail_instance.set_p('height', height)
@@ -31,6 +33,8 @@ while not failure:
     max_cf = sail_instance.get_cf()
     for direction in range(360):
         forcemag = max_cf * 0.5 * 1.225 * (max_windspeed**2) * chord * height
+        if forcemag > maxload:
+            maxload = forcemag
         force = np.array(([ forcemag*np.sin(direction*np.pi/180), forcemag*np.cos(direction*np.pi/180) ]))
         ok = ContLoad.CheckContainer(force, height/2, Stackheight, SF=SF)
         if not ok:
@@ -41,5 +45,6 @@ while not failure:
     print("Chord: ", chord)
     print("Surface area: ", sail_instance.get_p('area'))
     print("Failure: ", failure)
+    print("Max load: ", maxload)
     print()
     height += height_step
