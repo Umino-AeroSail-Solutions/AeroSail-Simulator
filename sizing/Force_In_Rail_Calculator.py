4ad2f604f33cb88ae0f8f3484ae67fac2b832578
l-w-k <elfferichluuk@gmail.com>
2025-02-13 20:13:20 +0100

    suicide

--- sizing/Force_In_Rail_Calculator.py
+++ sizing/Force_In_Rail_Calculator.py
@@ -4,7 +4,7 @@ from sympy import Symbol
 import sympy
 x2 = Symbol('x2')
 M = 2000000
-H = 30
+H = 8
 
 ####################################################################
 #Inputs: coordinate of P1, P2, P4, mass of mast M, height of mast H#
@@ -12,7 +12,7 @@ H = 30
 
 P1 = [0,1]
 P2 = [2,1]
-P3 = [40,11]
+P3 = [6,2]
 P4 = [6,0]
 
 L_Bot = math.sqrt((P4[0]-P1[0])**2 + (P4[1]-P1[1])**2)
@@ -27,7 +27,7 @@ def y1(l):
 
 def solve_for_x2(l):
     x2_Value = 0
-    solutions = solve(abs(P2[0] - P1[0]) - (sympy.sqrt(pow((x2 - l),2) + pow((((P3[1] - P2[1])/(P3[0] - P2[0]) * x2 + P2[1]) - ((P4[1] - P1[1])/(P4[0] - P1[0]) * l * math.cos(alpha) + P1[1])), 2))), x2)
+    solutions = solve(D**2 - ((x2 - l * math.cos(alpha))**2 + ((((P3[1] - P2[1])/(P3[0] - P2[0]) * (x2-P2[0]) + P2[1]) - ((P4[1] - P1[1])/(P4[0] - P1[0]) * l * math.cos(alpha) + P1[1])))**2 ), x2)
     #print(solutions)
     for solution in solutions:
         if x2_Value < solution:
@@ -53,7 +53,7 @@ while l < L_Bot:
 
     x1 = l * math.cos(alpha)
 
-    phi = abs(math.atan((y2(solve_for_x2(l)) - y1(l))/(solve_for_x2(l) - x1)))
+    phi = math.atan((y2(solve_for_x2(l)) - y1(l))/(solve_for_x2(l) - x1))
     print(phi)
 
     R2 = (M * 9.81 * H * math.sin(phi)) / D * math.cos(90 - phi + beta)
