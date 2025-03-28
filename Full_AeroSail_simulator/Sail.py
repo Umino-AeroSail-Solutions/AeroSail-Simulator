470d498d75b59e342538f71d459b1e8c1438f118
Andrés Blanquer <maquinsa101abb@gmail.com>
2025-03-29 00:42:44 +0100

    Added TWA calculation

--- Full_AeroSail_simulator/Sail.py
+++ Full_AeroSail_simulator/Sail.py
@@ -376,35 +376,51 @@ class Sail_Class():
         cf = np.sqrt(cl ** 2 + cd ** 2)
         return cf
 
-    def plot_optimal_values_polar_with_thrust(self, AWA_range, thrust_min, thrust_max, windspeedkt, radial_angles):
+    def plot_optimal_values_polar_with_thrust(self, TWA_range, thrust_min, thrust_max, windspeedkt, radial_angles, shipspeed=0):
         '''Plots the optimal thrust for an Apparent Wind Angle (AWA) range in a high-resolution polar graph'''
         optimal_thrusts = []
+        aws = []
+        shipspeed = shipspeed / 1.944
         windspeed = windspeedkt / 1.944
-        for AWA in AWA_range:
+        for TWA in TWA_range:
+            wind_back = -abs(windspeed * np.cos(TWA) + shipspeed)
+            wind_side = abs(windspeed * np.sin(TWA))
+            AWA = np.arctan(wind_side/wind_back)
+            AWS = np.sqrt(wind_side ** 2 + wind_back ** 2)
             opt_alpha, opt_flap = self.get_opt_pos(AWA)
-            optimal_thrusts.append(self.cts.max() * 0.5 * 1.225 * (windspeed ** 2) * self.height * self.chord)
-
+            optimal_thrusts.append(self.cts.max() * 0.5 * 1.225 * (AWS ** 2) * self.height * self.chord)
+            aws.append(AWA)
+        # print(aws)
         optimal_thrusts = np.array(optimal_thrusts)
-        thrust_min_array = np.array([thrust_min] * len(AWA_range))  # Array for thrust_min
-        thrust_max_array = np.array([thrust_max] * len(AWA_range))  # Array for thrust_max
+        thrust_min_array = np.array([thrust_min] * len(TWA_range))  # Array for thrust_min
+        thrust_max_array = np.array([thrust_max] * len(TWA_range))  # Array for thrust_max
 
+        print("Average thrust is: ", str(np.average(optimal_thrusts)))
         # Polar plot for optimal thrust with thrust_min and thrust_max
+        # a = plt.subplot(111, polar=True)
+        # a.plot(TWA_range, aws, 'g-', label='AWS')
         ax = plt.subplot(111, polar=True)
-        ax.plot(AWA_range, optimal_thrusts, 'g-', label='Optimal thrust')
-        ax.plot(AWA_range, thrust_min_array, 'm--', label='Requirement ASV2-STK-02b')
-        ax.plot(AWA_range, thrust_max_array, 'c--', label='Requirement ASV2-STK-02')
+        ax.plot(TWA_range, optimal_thrusts, 'g-', label='Optimal thrust')
+        ax.plot(TWA_range, thrust_min_array, 'm--', label='Requirement ASV2-STK-02b')
+        ax.plot(TWA_range, thrust_max_array, 'c--', label='Requirement ASV2-STK-02')
         ax.set_theta_zero_location('N')
         ax.set_theta_direction(-1)
         ax.set_rlabel_position(-22.5)  # Move radial labels to prevent overlap
-        ax.set_title('Optimal thrust vs AWA at ' + str(windspeedkt) + ' kt', va='bottom')
+        ax.set_title('Optimal thrust vs TWA at ' + str(windspeedkt) + ' kt' + ', shipspeed = '+ str(shipspeed*1.944) + 'kt', va='bottom')
+        # a.set_theta_zero_location('N')
+        # a.set_theta_direction(-1)
+        # a.set_rlabel_position(-22.5)  # Move radial labels to prevent overlap
+        # a.set_title("AWS vs TWA", va='bottom')
         for angle in radial_angles:
             ax.axvline(np.radians(angle), color='k', linestyle='--')  # Adding radial lines
 
         # Move the legend below the graph
         ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.1), ncol=2)
+        # a.legend(loc='upper center', bbox_to_anchor=(0.5, -0.1), ncol=2)
 
         plt.tight_layout()
         plt.show()
+        return np.average(optimal_thrusts)
 
 
 # TESTING CODE -------------------------------------------------
@@ -431,4 +447,6 @@ Sail.load_interpolation('Data/interpolationCR4sail_XFLR5.npz')
 # Sail.load_interpolation('Data/interpolationCR4sail_XFLR5.npz')
 # Sail.plot_optimal_values(np.arange(np.radians(5), np.radians(180), np.radians(0.01)))
 # Sail.plot_optimal_values_polar(np.arange(np.radians(-180), np.radians(180), np.radians(0.01)))
-Sail.plot_optimal_values_polar_with_thrust(np.arange(np.radians(-180), np.radians(180), np.radians(0.01)), 5000, 15000, 20, [30,150])
+
+# Lol
+Sail.plot_optimal_values_polar_with_thrust(np.arange(np.radians(-180), np.radians(180), np.radians(0.01)), 5000, 15000, 23, [30,150], shipspeed=22)
