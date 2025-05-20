a7bc3790b240da971710e277ffff9b39ebe77880
l-w-k <elfferichluuk@gmail.com>
2025-05-20 19:21:06 +0200

    Completed the functions

--- bwuuop/acc_at_any_point.py
+++ bwuuop/acc_at_any_point.py
@@ -68,6 +68,8 @@ def get_acc(x,y,z):
     a_y = f_beta *(C_YG * g * np.sin(theta) +C_YS * a_sway - C_YR* a_roll * (z-R))
 
     a_z = f_beta * (C_ZH * a_heave + C_ZR*a_roll * y - C_ZP * a_pitch * (x-0.45*L))
+    
+    return [a_x,a_y,a_z]
 
 
 def get_acc_env(x,y,z):
@@ -86,4 +88,15 @@ def get_acc_env(x,y,z):
     a_z_env_pitch = np.sqrt(a_heave**2 + ((0.95+np.exp(-L/15))*a_pitch*(1.08*x-0.45*L))**2)
     
     a_z_env_roll = np.sqrt(a_heave**2 + (1.2*a_roll*y)**2)
+
+    envelope = []
+    # load cases
+    envelope.append([a_x_env, 0, a_z_env_pitch])
+    envelope.append([a_x_env, 0, -a_z_env_pitch])
+    envelope.append([0, a_y_env, a_z_env_roll])
+    envelope.append([0, a_y_env, -a_z_env_roll])
+    envelope.append([0.6 * a_x_env, 0.6 * a_y_env, a_z_env])
+    envelope.append([0.6 * a_x_env, 0.6 * a_y_env, -a_z_env])
+
+    return envelope
     
