8864de075477ac6db85e17794e8f308841f0d8bf
Andrés Blanquer <maquinsa101abb@gmail.com>
2025-07-23 19:50:14 +0200

    Sized pulley beam 2

--- sizing/Mast sizing/mast segment 3/Pulley beam sizing.py
+++ sizing/Mast sizing/mast segment 3/Pulley beam sizing.py
@@ -3,15 +3,26 @@ import beam_sizer_copy as bs
 
 alu_6063_T66 = bs.Material("alu_6063_T66",Density=2710, YieldStrength=200000000.0, max_shear=152000000.0)
 
-Pulley_beam = bs.Square_beam(0.274, 0.06, 0.06, material=alu_6063_T66)
+Pulley4_beam = bs.Square_beam(0.274, 0.06, 0.06, material=alu_6063_T66)
 
 aeeoplatform_weight = 114
 Mast_segment_4_weight = 195 + aeeoplatform_weight
 
 total_pulley_load = 2*Mast_segment_4_weight*9.81/2
-print("Total pulley load: ", total_pulley_load, "N")
+print("Total pulley 4 load: ", total_pulley_load, "N")
 
 shear = [total_pulley_load,0]
-moment = [0, total_pulley_load*Pulley_beam.length/2]
+moment = [0, total_pulley_load*Pulley4_beam.length/2]
 
-Pulley_beam.size_profile(shear[0], shear[1], 0, moment[0], moment[1], 1.5, 1.5/1000,plot_convergence=True)
+Pulley4_beam.size_profile(shear[0], shear[1], 0, moment[0], moment[1], 1.5, 1.5/1000,plot_convergence=True)
+
+Mast_segment_3_weight = 540 + aeeoplatform_weight
+total_pulley_load = 2*Mast_segment_3_weight*9.81/2 + 4*Mast_segment_4_weight*9.81/2
+print("Total pulley 3 load: ", total_pulley_load, "N")
+
+Pulley3_beam = bs.Square_beam(0.438, 0.06, 0.06, material=alu_6063_T66)
+
+shear = [total_pulley_load/2,0]
+moment = [0, total_pulley_load*Pulley3_beam.length/4]
+
+Pulley4_beam.size_profile(shear[0], shear[1], 0, moment[0], moment[1], 1.5, 3/1000,plot_convergence=True)
