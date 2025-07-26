b17ca6235995b246e0a7952ceb511aa4ac557813
Andrés Blanquer <maquinsa101abb@gmail.com>
2025-07-26 16:02:02 +0200

    Finished pulley beam sizing

--- sizing/Mast sizing/mast segment 3/Pulley beam sizing.py
+++ sizing/Mast sizing/mast segment 3/Pulley beam sizing.py
@@ -25,4 +25,16 @@ Pulley3_beam = bs.Square_beam(0.438, 0.06, 0.06, material=alu_6063_T66)
 shear = [total_pulley_load/2,0]
 moment = [0, total_pulley_load*Pulley3_beam.length/4]
 
-Pulley4_beam.size_profile(shear[0], shear[1], 0, moment[0], moment[1], 1.5, 3/1000,plot_convergence=True)
+Pulley3_beam.size_profile(shear[0], shear[1], 0, moment[0], moment[1], 1.5, 3/1000,plot_convergence=True)
+
+Mast_segment_2_weight = 788.4 + aeeoplatform_weight
+total_pulley_load = 2*Mast_segment_2_weight*9.81/2 + 4*Mast_segment_3_weight*9.81/2 - 2*Mast_segment_3_weight*9.81/2
+print("Total pulley 2 load: ", total_pulley_load, "N")
+
+Pulley2_beam = bs.Square_beam(0.642, 0.06, 0.06, material=alu_6063_T66)
+
+shear = [total_pulley_load/3,0]
+moment = [0, total_pulley_load*Pulley3_beam.length/6]
+
+Pulley2_beam.size_profile(shear[0], shear[1], 0, moment[0], moment[1], 1.5, 3/1000,plot_convergence=True)
+
