7ccb37af1f66c633ee9203c055388b6c3761c3b7
akudohtudelft <akudohliang@tudelft.nl>
2025-11-18 19:14:14 +0100

    starting floor beam analysis

---
+++ railteam/floorbeamloads.py
@@ -0,0 +1,5 @@
+def findfloorbeamloads(x, r1d, r2d, r1l, r2l):
+    #assume lateral forces are positive in the direction of erection
+    # AXIAL FORCES are constant throughout the beam
+    force_axial = r2l -r1
+
